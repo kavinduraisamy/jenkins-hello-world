7aa35c4b946e77853017f9e528dd4958211d3eff
Kavin <kavinduraisamy91@gmail.com>
2022-05-29 13:55:49 +0530

    Intiial commit

---
+++ src/main/java/com/kavinduraisamy91/jenkins/JenkinsHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.kavinduraisamy91.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHelloWorldApplication.class, args);
+	}
+
+}
